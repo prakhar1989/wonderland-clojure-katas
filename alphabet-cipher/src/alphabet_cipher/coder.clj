a4b1edb30160236e61e5ee27e5c77277030400d8
Prakhar Srivastav <prakhar1989@gmail.com>
2015-01-18 08:40:33 +0300

    Finished decoder

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,33 @@
 (ns alphabet-cipher.coder)
 
-(defn encode [keyword message]
-  "encodeme")
+(def alphabets (seq "abcdefghijklmnopqrstuvwxyz"))
 
-(defn decode [keyword message]
+(defn repeat-string [string n]
+  "repeats a string n times"
+  (loop [xs string ys [] i n]
+    (cond
+      (zero? i) (apply str ys)
+      (empty? xs) (recur string ys i)
+      :else (recur (rest xs) 
+                   (conj ys (first xs)) 
+                   (dec i)))))
+
+(defn rotate-string [string n]
+  "rotates a string n times to the right"
+  (let [len (count string)
+        [xs ys] (split-at (mod n len) string)]
+    (apply str (concat ys xs))))
+
+(defn get-encoded-char [c x]
+  "return encoded char for char c and x cipher char"
+  (let [i (.indexOf alphabets c)
+        j (.indexOf alphabets x)
+        rot (rotate-string alphabets i)]
+    (first (rotate-string rot j))))
+
+(defn encode [secret msg]
+  (let [cipher (repeat-string secret (count msg))]
+    (apply str (map get-encoded-char msg cipher))))
+
+(defn decode [secret msg]
   "decodeme")
