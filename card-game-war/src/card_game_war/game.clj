5fd0f853017d235130d476b0a3a8467378cb38c1
Prakhar Srivastav <prakhar1989@gmail.com>
2015-01-24 13:34:04 +0300

    Finished card game war

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -1,13 +1,39 @@
-(ns card-game-war.game)
+(ns card-game-war.game
+  (:gen-class))
 
-;; feel free to use these cards or use your own data structure
 (def suits [:spade :club :diamond :heart])
-(def ranks [2 3 4 5 6 7 8 9 :jack :queen :king :ace])
-(def cards
-  (for [suit suits
-        rank ranks]
-    [suit rank]))
+(def ranks [2 3 4 5 6 7 8 9 10 :jack :queen :king :ace])
+(def cards (for [suit suits rank ranks] [suit rank]))
+(def card-value {:jack 11 :queen 12 :king 13 :ace 14
+                 :heart 4 :diamond 3 :club 2 :spade 1})
 
-(defn play-round [player1-card player2-card])
+(defn play-round [card1 card2]
+  "returns true if card1 is higher than card2. false otherwise"
+  (let [[s1 r1] card1 
+        [s2 r2] card2
+        v1 (if (keyword? r1) (r1 card-value) r1)
+        v2 (if (keyword? r2) (r2 card-value) r2)]
+    (if (= r1 r2) 
+      (> (s1 card-value) (s2 card-value))
+      (> v1 v2))))
 
-(defn play-game [player1-cards player2-cards])
+(defn deal [cards]
+  "shuffles the card amongst 2 players"
+  (apply map vector (partition 2 (shuffle cards))))
+
+(defn play-game [[player1-cards player2-cards]]
+  "returns true if player1 wins, false if player2 wins"
+  (let [card1 (first player1-cards)
+        card2 (first player2-cards)]
+    (cond
+      (nil? card1) false
+      (nil? card2) true
+      (play-round card1 card2) ;; p1 wins round
+        (play-game [(conj (rest player1-cards) card1 card2) 
+                    (rest player2-cards)])
+      :else ;; p2 wins round
+        (play-game [(rest player1-cards) 
+                    (conj (rest player2-cards) card1 card2)]))))
+
+(play-game (deal cards))
+
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -2,18 +2,39 @@
   (:require [clojure.test :refer :all]
             [card-game-war.game :refer :all]))
 
-
-;; fill in  tests for your game
 (deftest test-play-round
   (testing "the highest rank wins the cards in the round"
-    (is (= 0 1)))
-  (testing "queens are higer rank than jacks")
-  (testing "kings are higer rank than queens")
-  (testing "aces are higer rank than kings")
-  (testing "if the ranks are equal, clubs beat spades")
-  (testing "if the ranks are equal, diamonds beat clubs")
-  (testing "if the ranks are equal, hearts beat diamonds"))
+    (is (play-round [:spade 10] [:spade 2])))
+
+  (testing "the highest rank wins the cards in the round"
+    (is (not (play-round [:spade 2] [:spade 8]))))
+
+  (testing "queens are higer rank than jacks"
+    (is (play-round [:club :queen] [:club :jack])))
+
+  (testing "kings are higer rank than queens"
+    (is (play-round [:heart :king] [:heart :queen])))
+
+  (testing "aces are higer rank than kings"
+    (is (play-round [:spade :ace] [:spade :king])))
+
+  (testing "if the ranks are equal, clubs beat spades"
+    (is (play-round [4 :club] [4 :spade])))
+
+  (testing "if the ranks are equal, diamonds beat clubs"
+    (is (play-round [3 :diamond] [3 :club])))
+  
+  (testing "if the ranks are equal, hearts beat diamonds"
+    (is (play-round [5 :heart] [5 :diamond]))))
 
 (deftest test-play-game
-  (testing "the player loses when they run out of cards"))
+  (testing "the player loses when they run out of cards"
+    (let [player1-cards [[:heart 4] [:club 2]]
+          player2-cards [[:club 5] [:diamond 7]]]
+      (is (play-game [player2-cards player1-cards]))
+      (is (not (play-game [player1-cards player2-cards]))))
+    (let [player1-cards [[:heart 4] [:club 2]]
+          player2-cards [[:heart 2] [:club 7]]]
+      (is (play-game [player2-cards player1-cards]))
+      (is (not (play-game [player1-cards player2-cards]))))))
 
